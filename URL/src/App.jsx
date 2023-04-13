408258c9d1006643777228502751fa6155268013
Aymane <aymanelgenouni10@gmail.com>
2023-04-13 18:26:50 +0000

    first commit

---
+++ URL/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+            <h1 className='text-red-900 text-9xl'>Hello</h1>
+    </div>
+  )
+}
+
+export default App
